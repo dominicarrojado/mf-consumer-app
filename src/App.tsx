d9875f2b27a19775cf31240f2253d2ea689697bf
Dominic Arrojado <dominicarrojado@gmail.com>
2025-04-05 12:17:03 +0800

    set up fontsource

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import { ErrorBoundary } from 'react-error-boundary';
-import './App.css';
 import { lazy, Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/500-italic.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/600-italic.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400-italic.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/500-italic.css';
+import './App.css';
 
 const Provider = lazy(() => import('provider'));
 
@@ -12,7 +20,7 @@ const App = () => {
           <Provider />
         </Suspense>
       </ErrorBoundary>
-      <h1 className="mt-auto py-5 text-center text-3xl font-bold">
+      <h1 className="mt-auto py-5 text-center text-3xl font-medium">
         I&apos;m the consumer app
       </h1>
     </div>
